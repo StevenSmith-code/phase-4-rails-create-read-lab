d4cf4bed0da8691096bbb3e75e4f241f0a7fbf05
Steven <digitalexile99@gmail.com>
2023-04-19 12:25:18 -0500

    commit tree 715b3cf1e97cafb98fbb884cd595f293abc9fe77

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: [:create, :show, :index]
+
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      render json: plant
+    else
+      render json: {error: "Plant not found"} , status: :not_found
+    end
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
 end
